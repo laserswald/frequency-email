1ce1ef2a0de20883b91e8eda68e3d1586381d682
Ben Davenport-Ray <laserdude11@gmail.com>
2011-02-26 19:31:37 +0000

    Fixed the setup.py so that icons are placed in there.

--- src/setup.py
+++ src/setup.py
@@ -7,9 +7,25 @@ setup(
 	version = "0.1",
 	windows = [
 		{
-			"script": "src/frequency.py",
-			"icon_resources": [(1, "src/icons/Frequency.ico")]
+			"script": "frequency.py",
+			"icon_resources": [(1, "icons/Frequency.ico")]
 		}
 	],
 	script_args = ["py2exe"],
+	data_files = [ ("icons",["icons/Frequency.ico",
+							 'icons/email.png',
+							 'icons/email_add.png',
+							 'icons/email_delete.png',
+							 'icons/email_attach.png',
+							 'icons/email_edit.png',
+							 'icons/door_in.png',
+							 'icons/user.png',
+							 'icons/wrench.png',
+							 ])
+	],
+	options = {
+		"py2exe": {
+			"dist_dir":"../dist"
+		}
+	}
 )
