ff5561a70cdfb0275ccffe7c72697b74b406d0c2
Ben Davenport-Ray <laserdude11@gmail.com>
2011-02-26 18:52:09 +0000

    Fixed a lot of stuff!

--- src/frequency.py
+++ src/frequency.py
@@ -1,12 +1,7 @@
-def main(toolkit):
-	if toolkit == 'tk':
-		import gui.tk.mainWindow as mainWin
-	elif toolkit == 'wx':
-		import gui.wxgui.mainWindow as mainWin
-	elif toolkit == 'gtk':
-		import gui.gtk.mainWindow as mainWin
-	
-	mainWin.start()
+import logic.mainLogic
+
+def main():
+	mwl = logic.mainLogic.MainWindowLogic()
 	
 if __name__ == "__main__":
-	main('wx')
+	main()
